e4a620e1d6e9b319297247d89d95d9043acc2354
zhoujiajie <zhoujiajie@internetapi.cn>
2020-06-27 23:01:05 +0800

    convert between libp2p ID and tdm ID

--- p2p/types.go
+++ p2p/types.go
@@ -2,7 +2,10 @@ package p2p
 
 import (
 	"github.com/bdware/tendermint/p2p/conn"
+	peer2 "github.com/libp2p/go-libp2p-core/peer"
 )
 
 type ChannelDescriptor = conn.ChannelDescriptor
 type ConnectionStatus = conn.ConnectionStatus
+
+type Libp2pID = peer2.ID
