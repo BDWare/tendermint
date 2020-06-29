9cb589c22f3acd22fe32bf360782263f83609d3c
puck1 <yaodidu@gmail.com>
2020-06-29 14:54:37 +0800

    update scripts

---
+++ tendermint_app/clean.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# badger db
+rm -rf /tmp/badger > /dev/null 2>&1
+
+# addrbook
+rm example/config/addrbook.json  > /dev/null 2>&1
+
+# db
+rm -rf example/data/*.db > /dev/null 2>&1
+
+# wal
+rm -rf example/data/*.wal > /dev/null 2>&1
+
+# reset priv validator state using backup file
+cp example/data/priv_validator_state.json.bak example/data/priv_validator_state.json
--- tendermint_app/genconf.sh
+++ tendermint_app/genconf.sh
@@ -2,3 +2,5 @@
 
 # Be careful, this will overwrite existing config files.
 TMHOME="$PWD/example" tendermint init
+
+cp example/data/priv_validator_state.json example/data/priv_validator_state.json.bak
